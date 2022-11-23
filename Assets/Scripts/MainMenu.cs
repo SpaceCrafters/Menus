d4ebff6aeed1258dceb0f52e24c3d641cacbdaa2
Agnès <115895830+AgnesG19@users.noreply.github.com>
2022-11-23 22:12:27 +0100

    Add files via upload
    Menu Principal mejorado

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("MenuGame");
+    }
+
+    public void OpcionesJuego()
+    {
+        SceneManager.LoadScene("OpcionesJuego");
+    }
+    public void CreditosJuego()
+    {
+        SceneManager.LoadScene("CreditosJuego");
+    }
+
+
+    public void SalirJuego()
+    {
+        Debug.Log("Salir Juego");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/SoundMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMenu : MonoBehaviour {
+    public AudioSource sound;
+    public AudioClip button;
+
+    public void SoundButton() //Llamamos a estafuncion a traves del Event Trigger
+    {
+        sound.clip = button;
+        //se activa y desactiva
+        sound.enabled = false;
+        sound.enabled = true;
+    }
+ 
+}
